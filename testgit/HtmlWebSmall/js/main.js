1df38968fcf1ba4e20d5c387f4f3fbf4013455de
tang <1512019853@qq.com>
2017-08-10 19:09:09 +0800

    第一次HtmlWebSmall 2017.08.10

---
+++ testgit/HtmlWebSmall/js/main.js
@@ -0,0 +1,9 @@
+function icon(){
+	var now = document.getElementById("accordion");
+	var main = document.getElementById("contents-left-m");
+	if(now.style.display=="block"){
+		now.style.display="none";
+	}else{
+		now.style.display="block";
+	}
+}
